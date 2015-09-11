4e9ac8ff1f84b9656c7878fb3f01618c298eebb7
JoshuaBeckerASU <jdbecke3@asu.edu>
2015-09-10 21:39:05 -0700

    This Time should be better
    GUI's

---
+++ GamePanel.java
@@ -0,0 +1,89 @@
+/* 
+Name: Game Panel 
+Author: Joshua Becker
+Create On: 9/9/15
+Updated On: 9/10/15
+Contributors:
+ */
+
+import java.awt.*;
+import java.awt.event.*; 
+import javax.swing.*;
+import javax.swing.JFrame;
+
+public class GamePanel extends JPanel 
+{
+	private JButton exit;
+    private JLabel label; 
+	private JFrame gameFrame;
+	
+    public GamePanel(JFrame incGameFrame)// constructer
+    {
+		gameFrame = incGameFrame;
+		
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();// geting size of screen
+		int width = gd.getDisplayMode().getWidth();
+		int height = gd.getDisplayMode().getHeight();
+		
+		gameFrame.setLayout(new FlowLayout());
+		gameFrame.setBackground(Color.lightGray);
+		gameFrame.setPreferredSize(new Dimension(width/2, height/2));
+		gameFrame.setLocation(width/4, height/4); // centering window
+		
+		createComponents();
+		
+		addActionListeners();
+		
+		addElements();
+	}
+	
+	/**addElements
+	* add components to panels and
+	* adds panels to Frame
+	* JoshuaBecker
+	**/
+	private void addElements()
+	{
+		gameFrame.add(label);
+		gameFrame.add(exit);
+	}
+	
+	/**createComponents
+	* creates components and gives them
+	* default values.
+	* JoshuaBecker
+	**/
+	private void createComponents()
+	{
+		label = new JLabel(" Work In Progress");
+		exit = new JButton("Exit To Desktop");
+	}
+
+	/**addActionListeners
+	* adds ActionListener, which wait till
+	* an action is Performed then sends 
+	* a event to the type of listener.
+	* JoshuaBecker
+	**/
+	private void addActionListeners()
+	{
+		exit.addActionListener(new ButtonListener());
+	}
+
+	/**Listeners
+	* Once an event occurs the program goes here
+	* and decides what to do with each event.
+	* JoshuaBecker
+	**/
+	private class ButtonListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+			String command = event.getActionCommand();
+			if(command.equals( "Exit To Desktop"))
+			{
+				System.exit(0);
+			}
+		}  
+	}
+}
